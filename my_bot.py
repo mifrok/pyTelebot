7c6cdda97ff6acb734b2b34527d2b80442a5679e
mifrok <mifra@mail.ru>
2022-05-23 17:35:22 +0700

    added_async_bot

---
+++ my_bot.py
@@ -0,0 +1,70 @@
+import telebot
+import time
+import re
+from multiprocessing import *
+import schedule
+
+with open('token.txt', 'r') as f:
+    token = f.read()
+bot = telebot.TeleBot(token)
+chat_id = '850855659'
+web_regex_mask = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
+
+
+def sender_username_detector(username, first_name, last_name):
+    if username is not None:
+        return username
+    elif first_name is not None:
+        return first_name
+    elif last_name is not None:
+        return last_name
+    else:
+        return "NoName"
+
+
+def start_process():  # Запуск Process
+    p1 = Process(target=P_schedule.start_schedule, args=()).start()
+
+
+class P_schedule():  # Class для работы с schedule
+    def start_schedule():  # Запуск schedule
+        ######Параметры для schedule######
+        schedule.every().day.at("11:02").do(P_schedule.send_message1)
+        schedule.every(1).minutes.do(P_schedule.send_message2)
+        ##################################
+
+        while True:  # Запуск цикла
+            schedule.run_pending()
+            time.sleep(1)
+
+    ####Функции для выполнения заданий по времени
+    def send_message1():
+        bot.send_message(chat_id, 'Отправка сообщения по времени')
+
+    def send_message2():
+        bot.send_message(chat_id, 'Отправка сообщения через определенное время')
+    ################
+
+
+###Настройки команд telebot#########
+@bot.message_handler()
+def start_message(message):
+    web_site = re.findall(web_regex_mask, message.text)
+    if web_site:
+        print(message.chat.id)
+        from_username = sender_username_detector(message.from_user.username, message.from_user.first_name,
+                                                 message.from_user.last_name)
+        bot.send_message(message.chat.id,
+                         f'Ссылка пользователя {from_username} на {web_site[0]} удалена')
+        bot.delete_message(message.chat.id, message.id)
+
+
+#####################
+
+
+if __name__ == '__main__':
+    start_process()
+    try:
+        bot.infinity_polling()
+    except:
+        pass
